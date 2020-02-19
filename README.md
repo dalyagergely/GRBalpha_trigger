e43134e8f2051577ce761e656723455f79130dda
dalyagergely <dalyagergely@gmail.com>
2020-02-19 11:44:59 -0600

    Implemented dynamically changeable stack and other comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# GRBalpha_trigger
+# GRBalpha trigger
+
+VHDL code of the trigger algorithm for the GRBalpha satellite.
--- trigger.vhd
+++ trigger.vhd
@@ -11,13 +11,15 @@ use ieee.math_real.all;
 entity TrigCircuit is
     port (
         -- we need to figure out what will be the possible values for EMIN, EMAX, PH and K
-        EMIN_CHOOSE         : in std_logic_vector (11 downto 0);  
+        EMIN_CHOOSE         : in std_logic_vector (7 downto 0);  
         -- it will enable us to choose between pre-defined EMIN values
-        EMAX_CHOOSE         : in std_logic_vector (11 downto 0);
-        T_CHOOSE            : in std_logic_vector (3 downto 0);
+        EMAX_CHOOSE         : in std_logic_vector (7 downto 0);
+        T_CHOOSE            : in std_logic_vector (2 downto 0);
         -- T is the integration time of the counter before the stack
         K_CHOOSE            : in std_logic_vector (7 downto 0);
         -- K is the multiplicator for the background i.e. we compare (S-B)^2 to K*B
+        WIN_CHOOSE          : in std_logic_vector (3 downto 0);
+        -- Choose the time window for signal and background accumulation
         PH                  : in std_logic_vector (11 downto 0);
         -- The output of the gamma detector is 12 bit ADC data
         CLK                 : in std_logic;
@@ -36,69 +38,115 @@ end TrigCircuit;
 
 architecture TrigArch of TrigCircuit is
 
-type shift_register is array (0 to 15) of unsigned;  -- how large should the stack be?
+type shift_register is array (0 to 4095) of unsigned;
 
 constant CLK_TIME_MS    : integer := 10; -- what is the clock frequency for our FPGA?
 constant CLK_FREQ_MHZ   : integer := 1/CLK_TIME_MS;
 
--- signal EMIN : std_logic_vector (...);
--- signal EMAX : std_logic_vector (...);
--- signal K : std_logic_vector (...);
-signal T            : std_logic_vector (15 downto 0);
+signal EMIN         : std_logic_vector (15 downto 0);
+signal EMAX         : std_logic_vector (15 downto 0);
+signal K            : std_logic_vector (15 downto 0);
+signal T            : std_logic_vector (9 downto 0);
+signal SIGWIN       : std_logic_vector (15 downto 0);
+signal BGWIN        : std_logic_vector (15 downto 0);
 signal ticks        : unsigned;
 signal millisecs    : unsigned;
-signal counter      : unsigned;  -- what count numbers do we realistically expect?
+signal counter      : unsigned;
 
 signal stack : shift_register := others => 0;
 
-variable accumulated_signal         : integer;
-variable accumulated_background     : integer;
+variable accumulated_signal         : unsigned;
+variable accumulated_background     : unsigned;
+variable n                          : unsigned;
+variable N                          : unsigned;
+variable step_counter               : unsigned := 0;
 
-signal comp1        : std_logic := '0'
-signal comp2        : std_logic := '0'
+signal stackfull    : std_logic := '0';
+signal comp1        : std_logic := '0';
+signal comp2        : std_logic := '0';
 
 begin
 
     with T_CHOOSE select 
-        T <= 32     when "0000",
-             64     when "0001",
-             128    when "0010",
-             256    when "0011",
-             512    when "0100",
-             1024   when "0101",
-             2048   when "0110",
-             4096   when "0111",
-             8196   when "1000",
-             16384  when "1001",
-             32768  when "1010",
-             65536  when "1011",
-             65536  when others;
+        T <= 32     when "000",
+             64     when "001",
+             128    when "010",
+             256    when "011",
+             512    when "100",
+             1024   when "101",
+             1024   when others;
+             
+    with WIN_CHOOSE select  -- signal windows in ms
+        SIGWIN <= 32    when "0000",
+                  64    when "0001",
+                  128   when "0010",
+                  256   when "0011",
+                  512   when "0100",
+                  1024  when "0101",
+                  2048  when "0110",
+                  4096  when "0111",
+                  8196  when "1000",
+                  16384 when "1001",
+                  32768 when "1010",
+                  65536 when "1011",
+                  65536 when others;
+                  
+    with WIN_CHOOSE select  -- background windows in ms
+        BGWIN <= 16384  when "0000" | "0001" | "0010" | "0011" | "0100",
+                 32768  when "0101" | "0110" | "0111",
+                 65536  when "1000" | "1001" | "1010" | "1011",
+                 65536  when others;
+
+-- So we have integration time options of 32-1024 ms, signal time windows of 32-65536 ms and 
+-- background time windows of 16384-65536 ms. In this case the possible values of n are between 
+-- 1-2048 and the possible values of N are 16-2048. Since we can not dynamically change the hardware 
+-- of the stack, we should impement a stack with the maximal size, i.e. 2*2048=4096, and then 
+-- dynamically change which parts are added to / subtracted from it.
+
+              
+    with K_CHOOSE select
+        K <= ... 
+     
+    with EMIN_CHOOSE select
+        EMIN <= ... 
+     
+    with EMAX_CHOOSE select
+        EMAX <= ... 
         
- -- with K_CHOOSE select
-     -- K <= ... 
-     
- -- with EMIN_CHOOSE select
-     -- EMIN <= ... 
-     
- -- with EMAX_CHOOSE select
-     -- EMAX <= ... 
+        
+-- I think that whenever we change one of the inputs of [EMIN_CHOOSE, EMAX_CHOOSE, T_CHOOSE, K_CHOOSE, WIN_CHOOSE], the stack should reset, to avoid some strange unwanted behaviour due to leftover count numbers somewhere, so:
+
+    Reset_After_Change : process (EMIN_CHOOSE, EMAX_CHOOSE, T_CHOOSE, K_CHOOSE, WIN_CHOOSE) is
+    begin
+        stack := others => 0;
+        stackfull <= '0';
+        step_counter := 0;
+        accumulated_signal := 0;
+        accumulated_background := 0;
+    end process Reset_After_Change;
+        
+        
+    n := SIGWIN / T;
+    N := BGWIN / T;
+        
      
     Clk_Proc : process (CLK) is
     begin
        if rising_edge (CLK) then
            ticks <= ticks + 1;
-           counter <= counter + unsigned(PH)
            
-           if ticks = CLK_TIME_MHZ - 1 then  -- count the milliseconds based on clk frequency
+           if EMIN < PH and EMAX > PH then  -- filter based on the energy
+               counter <= counter + 1;
+           end if;
+           
+           if ticks = CLK_FREQ_MHZ - 1 then  -- count the milliseconds based on clk frequency
                ticks <= 0;
                millisecs <= millisecs + 1;
            end if;   
             
            if millisecs = T then
-               -- reset millisecs
-               millisecs <= 0;
-               -- reset counter
-               counter <= 0;
+               millisecs <= 0;  -- reset millisecs
+               counter <= 0;  -- reset counter
            end if;    
              
        end if;
@@ -110,45 +158,46 @@ begin
        if millisecs = T then
            stack    <=  shift_right(stack, 1); -- shift to the right by one bit
            stack(0) <=  counter;
+           step_counter := step_counter + 1;
        end if;            
     end process Stepping;
-     
-     
-    S_And_B_Accumulation : process (millisecs) is
+    
+    
+    Stack_Full_Check : process (step_counter) is
     begin
-       if millisecs = T then
-           if stack(15) /= 0:  -- should have a more clever way to check whether the stack is full
-               accumulated_signal := accumulated_signal + stack(0) - stack(3)
-               accumulated_background := accumulated_background + stack(4) - stack(15)
-               -- how large should be the register for the signal and the noise?
-           end if;
-       end if;
+        if step_counter = n+N:
+            stackfull <= '1';
+        end if;
+    end process Stack_Full_Check
+    
+     
+    S_And_B_Accumulation : process (step_counter) is
+    begin
+       accumulated_signal := accumulated_signal + stack(0) - stack(n-1)
+       accumulated_background := accumulated_background + stack(n) - stack(n+N-1)
     end process S_And_B_Accumulation;
      
      
-    Comparison1 : process (millisecs) is
-       variable S, B, SmBsq : integer;
+    Comparison1 : process (step_counter) is
+       variable S, B, SmBsq : unsigned;
     begin
-       if millisecs = T then
-           S = accumulated_signal / 4;
-           B = accumulated_background / 12;
-           SmBsq = (S - B) ** 2;
+       S = accumulated_signal / n;
+       B = accumulated_background / N;
+       SmBsq = (S - B) ** 2;
             
-           if SmBsq > K*B then
-               comp1 <= '1';
-           else
-               comp1 <= '0';
-           end if;
-            
-       end if;
+       if SmBsq > K*B then
+           comp1 <= '1';
+       else
+           comp1 <= '0';
+       end if;            
     end process Comparison1;       
      
      
-    Comparison2 : process (millisecs) is
-       variable S, B : integer;
+    Comparison2 : process (step_counter) is
+       variable S, B : unsigned;
     begin
-        S = accumulated_signal / 4;
-        B = accumulated_signal / 12;
+        S = accumulated_signal / n;
+        B = accumulated_signal / N;
         
         if S > B then
             comp2 <= '1';
@@ -158,11 +207,17 @@ begin
     end process Comparison2;
     
     
-    Triggering : process (millisecs) is -- should have a more clever way to check whether the stack is full
-        if (comp1 = '1' and comp2 = '1' and CLEAR = '0' and stack(15) /= 0) then
-            TRIGGER <= '1'
+    Triggering : process (step_counter) is
+        if (comp1 = '1' and comp2 = '1' and CLEAR = '0' and stackfull = '1') then
+            TRIGGER <= '1';
+            -- reset the stack, the counter indicating whether the stack is full, accumulated_signal and accumulated_background
+            stack := others => 0;
+            stackfull <= '0';
+            step_counter := 0;
+            accumulated_signal := 0;
+            accumulated_background := 0;
         else
-            TRIGGER <= '0'
+            TRIGGER <= '0';
         end if;
     end process Triggering;
 
